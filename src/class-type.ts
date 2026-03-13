ff7396434880415c6e4e1304dd0f741074314367
Baptiste Pillot <baptiste@pillot.fr>
2026-03-13 09:07:23 +0100

    Revert Type args to any[] for TS mixin compatibility

--- src/class-type.ts
+++ src/class-type.ts
@@ -8,7 +8,7 @@ export type ObjectOrType<T extends object = object> = T | Type<T>
 
 export type StringObject = Record<string, string>
 
-export type Type<T extends object = object> = new (...args: unknown[]) => T
+export type Type<T extends object = object> = new (...args: any[]) => T
 
 const reservedClassNames: string[] = []
 
